3a9663a856493fd13753768b95964a306fcb52a3
zoeperezc <110468532+zoeperezc@users.noreply.github.com>
2023-08-02 10:25:22 -0300

    Update index.php: corrección/pureba con nacho

--- PlanetScale/index.php
+++ PlanetScale/index.php
@@ -1,7 +1,15 @@
 <?php
-$_ENV= parse_ini_file(’.env’) 
+$_ENV= parse_ini_file('.env');
 $mysqli = mysqli_init();
-$mysqli->ssl_set(NULL, NULL, "C:\AppServ\www\ProyectoAstrofisica_\cacert.pem", NULL, NULL);
+$mysqli->ssl_set(NULL, NULL, '../cacert.pem', NULL, NULL);
 $mysqli->real_connect($_ENV["HOST"], $_ENV["USERNAME"], $_ENV["PASSWORD"], $_ENV["DATABASE"]);
+
+echo "AAAAAAAAAAAAAAa";
+
+$result = $mysqli->query("SELECT * FROM test");
+while($row = $result->fetch_assoc()){
+    echo $row["id"] . " - " . $row["a"] . " - " . $row["b"];
+}
+
 $mysqli->close();
 ?>
