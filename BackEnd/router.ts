1db23378852d70441903f62fd3cb4bf73624a1f4
Paloma Monzon Borioli <110058621+PalomaMonzon@users.noreply.github.com>
2023-09-06 14:32:38 -0300

    fix

--- BackEnd/router.ts
+++ BackEnd/router.ts
@@ -4,6 +4,10 @@ import * as controllers from "./controllers";
 
 const router = Router();
 
-router.get("", controllers.asas);
+router.post("/signup", controllers.createUser); //user registration
+router.post("/login", controllers.logInUser); // login
+router.get("/user/:id", controllers.getUser); // ID user
+router.put("/user/:id", controllers.updateUser); // Update user info
+router.put("/user/:id/password", controllers.updatePassword); // Update user password
 
 export default router;
