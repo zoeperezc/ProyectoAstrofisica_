2c57953bb9ec8a58c62279de8dd0700d13720ee2
Paloma Monzon Borioli <110058621+PalomaMonzon@users.noreply.github.com>
2023-10-02 11:51:49 -0300

    consegi un eternet y biueno

--- BackEnd/router.ts
+++ BackEnd/router.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 
 import * as controllers from "./controllers";
+import { updateDONKINews } from "./cronJobs";
 
 const router = Router();
 
@@ -9,6 +10,14 @@ router.post("/login", controllers.logInUser); // login
 router.get("/user/:id", controllers.getUser); // id user
 router.put("/user/:id", controllers.updateUser); // update user info
 router.put("/user/:id/password", controllers.updatePassword); // update user password
-//router.put("/user/:id/image", controllers.); // user image to analize QUEEEEEEEEEE AAAAAAAAAAAAAAAAAAA
+router.put("/upload-image", controllers.); // user image to analize QUEEEEEEEEEE AAAAAAAAAAAAAAAAAAA
+//router.put("/libery", controllers.);
+
+// ruta /donki-news
+router.get('/donki-news', (req, res) => {
+  updateDONKINews(); 
+  res.status(200).json({ message: 'Actualizando noticias de DONKI' });
+});
+
 
 export default router;
