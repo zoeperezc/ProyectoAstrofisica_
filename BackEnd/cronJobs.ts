3389c1e74f9cb18d4564dcdf9d3cb11c1fde785e
Paloma Monzon Borioli <110058621+PalomaMonzon@users.noreply.github.com>
2023-10-31 10:40:16 -0300

    cronjobs nacho nachito + inteo te arreglo

--- BackEnd/cronJobs.ts
+++ BackEnd/cronJobs.ts
@@ -2,24 +2,24 @@ import cron from 'node-cron';
 import axios from 'axios';
 
 // Función para obtener y actualizar las noticias de DONKI
-async function updateDONKINews() {
+ export async function updateDONKINews() {
   try {
-    const apiKey = 'TU_API_KEY'; // Reemplazar API key de NASA DONKI
-    const startDate = 'yyyy-MM-dd'; // Especifica la fecha de inicio 
-    const endDate = 'yyyy-MM-dd'; // Especifica la fecha de fin 
-    
-    const response = await axios.get(`https://api.nasa.gov/DONKI/notifications?startDate=${startDate}&endDate=${endDate}&type=all&api_key=${apiKey}`);
+    const apiKey = 'Rh1lUF8pDz1LYGwyv1O6I3IYgTxsv0jAxR9LlQnX' ; 
 
-    // Aquí puedes manejar la respuesta de DONKI, por ejemplo, almacenarla en tu base de datos o memoria
+    const response = await axios.get(`https://api.nasa.gov/DONKI/notifications?api_key=${apiKey}`);
+
+    // Aquí puedes manejar la respuesta de DONKI
     const newsData = response.data;
     // Realiza las acciones necesarias para almacenar o actualizar los datos en tu aplicación
-    // ...
+
+    console.log(newsData); 
+   
   } catch (error) {
     console.error('Error al obtener noticias de DONKI:', error);
   }
 }
 
 // Programa una tarea para ejecutar updateDONKINews todos los días a una hora específica (por ejemplo, a las 2 AM)
-cron.schedule('0 2 * * *', () => {
+cron.schedule('30 0 * * *', () => {
   updateDONKINews();
 });
