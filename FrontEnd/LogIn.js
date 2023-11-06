21cca0694744ffc5c0679f11136bf6b17065ba71
Paloma Monzon Borioli <110058621+PalomaMonzon@users.noreply.github.com>
2023-11-06 15:12:17 -0300

    zozo

--- FrontEnd/LogIn.js
+++ FrontEnd/LogIn.js
@@ -1,42 +1,45 @@
 $(function() {
-	$(".btn").click(function() {
-		$(".form-signin").toggleClass("form-signin-left");
-    $(".form-signup").toggleClass("form-signup-left");
-    $(".frame").toggleClass("frame-long");
-    $(".signup-inactive").toggleClass("signup-active");
-    $(".signin-active").toggleClass("signin-inactive");
-    $(".forgot").toggleClass("forgot-left");   
-    $(this).removeClass("idle").addClass("active");
-	});
-});
+    $(".btn").click(function() {
+      $(".form-signin").toggleClass("form-signin-left");
+      $(".form-signup").toggleClass("form-signup-left");
+      $(".frame").toggleClass("frame-long");
+      $(".signup-inactive").toggleClass("signup-active");
+      $(".signin-active").toggleClass("signin-inactive");
+      $(".forgot").toggleClass("forgot-left");
+      $(this).removeClass("idle").addClass("active");
+    });
+  
+    document.getElementById('loginForm').addEventListener('submit', (e) => {
+      e.preventDefault();
+  
+      const email = document.getElementById('email').value;
+      const password = document.getElementById('password').value;
+  
+      fetch('/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ email, password })
+      })
+      .then(response => response.json())
+      .then(data => {
+        const message = document.getElementById('message');
+        if (data.message === "Login successful") {
+          message.innerHTML = `¡Bienvenido, ${data.user.username}!`;
+        } else {
+          message.innerHTML = data.message;
+        }
+      })
+      .catch(error => {
+        console.error(error);
+      });
+    });
+  });
+  
 
 const username = document.getElementById('username').value;
 const password = document.getElementById('password').value;
 const email = document.getElementById('mail').value;
 const password_1 = document.getElementById('password_1').value;
 const password_2 = document.getElementById('password_2').value;
-
-document.getElementById('loginForm').addEventListener('submit', (e) => {
-  e.preventDefault();
-
-  fetch('/login', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ mail, password })
-})
-.then(response => response.json())
-.then(data => {
-    if (data.message === "Login successful") {
-        const message = document.getElementById('message');
-        message.innerHTML = `¡Bienvenido, ${data.user.username}!`;
-    } else {
-        const message = document.getElementById('message');
-        message.innerHTML = data.message;
-    }
-})
-.catch(error => {
-    console.error(error);
-});
-});
