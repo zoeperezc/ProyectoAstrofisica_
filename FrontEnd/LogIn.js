227fefb73c07c750394c3bb376c03258b2f60883
Paloma Monzon Borioli <110058621+PalomaMonzon@users.noreply.github.com>
2023-11-06 15:20:00 -0300

    eso fue tan lindo

--- FrontEnd/LogIn.js
+++ FrontEnd/LogIn.js
@@ -12,7 +12,7 @@ $(function() {
     document.getElementById('loginForm').addEventListener('submit', (e) => {
       e.preventDefault();
   
-      const email = document.getElementById('email').value;
+      const username = document.getElementById('username').value;
       const password = document.getElementById('password').value;
   
       fetch('/login', {
@@ -36,7 +36,36 @@ $(function() {
       });
     });
   });
-  
+
+  document.getElementById('signupForm').addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const username = document.getElementById('username').value;
+    const email = document.getElementById('email').value;
+    const password_1 = document.getElementById('password_1').value;
+    const password_2 = document.getElementById('password_2').value;
+
+    fetch('/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ email, password })
+    })
+    .then(response => response.json())
+    .then(data => {
+      const message = document.getElementById('message');
+      if (data.message === "Login successful") {
+        message.innerHTML = `¡Bienvenido, ${data.user.username}!`;
+      } else {
+        message.innerHTML = data.message;
+      }
+    })
+    .catch(error => {
+      console.error(error);
+    });
+  });
+ 
 
 const username = document.getElementById('username').value;
 const password = document.getElementById('password').value;
