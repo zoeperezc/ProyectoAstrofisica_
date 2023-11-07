015c292023f815893bfed648d223b97fb38e6b90
Paloma Monzon Borioli <110058621+PalomaMonzon@users.noreply.github.com>
2023-11-07 14:24:42 -0300

    si

--- BackEnd/index.ts
+++ BackEnd/index.ts
@@ -1,18 +1,18 @@
 import express from "express";
-import router from "./router";
+import router from "./router"; 
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
-const app = express();
+const app = express(); 
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
 app.use(router);
-
-app.use("/", router); // Use the router for all routes
 
 const PORT: number = 3000;
 
 app.listen(PORT, () => {
   console.log("Server is alive on localhost:" + PORT);
 });
+
+export default router;
