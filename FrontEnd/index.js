816304f32417694cba9c8a834bb299d01c911b27
zoeperezc <110468532+zoeperezc@users.noreply.github.com>
2023-11-14 08:23:49 -0300

    fix: index

---
+++ FrontEnd/index.js
@@ -0,0 +1,3 @@
+const goHome = document.getElementById('home'); //no tiene funcion no importa
+const goLogIn = document.getElementById('logIn'); //redirecciona al log in
+const goAbout = document.getElementById('about'); //redirecciona al about ?? todavia no existe el log out
