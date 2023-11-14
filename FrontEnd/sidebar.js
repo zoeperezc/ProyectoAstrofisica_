1068c1e190a8fbf8b79e3cda41e93dfb3f09df56
zoeperezc <110468532+zoeperezc@users.noreply.github.com>
2023-11-14 08:45:45 -0300

    fix:sidebad

--- FrontEnd/sidebar.js
+++ FrontEnd/sidebar.js
@@ -19,4 +19,13 @@ function menuBtnChange() {
        }else {
          closeBtn.classList.replace("bx-menu-alt-right","bx-menu");//replacing the iocns class
        }
-      }            
+      }
+      
+const goIndice = document.getElementById('goIndice'); //te lleva al indice
+const goHistory = document.getElementById('goHistory'); //te lleva al history.html
+const goImage = document.getElementById('goImage'); //te lleva al upload-image.html
+const goSearch = document.getElementById('goSearch'); //te lleva al search.html
+const goNews = document.getElementById('goNewa'); //redirecciona al NASA-news.html
+const goApod = document.getElementById('goApod'); //te lleva a apod.html
+const goAbout = document.getElementById('goAbout'); //te lleva a un about que todavia no esta
+const LogOut = document.getElementById('LogOut'); //te log out
