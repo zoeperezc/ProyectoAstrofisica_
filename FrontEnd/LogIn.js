e28701c13b943d52915205a146fe963d05e846ce
zoeperezc <110468532+zoeperezc@users.noreply.github.com>
2023-11-17 10:52:50 -0300

    fix: LogIn.js

--- FrontEnd/LogIn.js
+++ FrontEnd/LogIn.js
@@ -12,69 +12,82 @@ $(function() {
   const register = document.getElementById('register');
   const logIn = document.getElementById('login');
 
+
+
   register.addEventListener('click', (e) => {
     e.preventDefault();
     e.stopPropagation();
 
-    const username = document.getElementById('username').value;
+    const username = document.getElementById('usernameSingIn').value;
     const mail = document.getElementById('mail').value;
     const password_1 = document.getElementById('password_1').value;
     const password_2 = document.getElementById('password_2').value;
-
-    fetch('http://localhost:3000/register', {
-      method: 'POST',
-      credentials: 'include',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        username: username,
-        mail: mail,
-        password: password_1,
-        passwordConfirm: password_2,
-      })
-    })
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        if (data.success) {
-          window.location.href = "http://localhost:3000/indice";
-        }
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  });
-
-
-  logIn.addEventListener('click', (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-
-    fetch('http://localhost:3000/login', {
-      method: 'POST',
-      credentials: 'include',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        username: username,
-        password: password,
-      })
-    })
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        if (data.success) {
-            window.location.href = "http://localhost:3000/indice";
-        }
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  });
-  
+    
+      if (password_1 === password_2) {
+          errorMessage.innerHTML = '';
+          fetch('http://localhost:3000/register', {
+            method: 'POST',
+            credentials: 'include',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              username: username,
+              mail: mail,
+              password: password_1,
+              passwordConfirm: password_2,
+            })
+          })
+            .then(response => response.json())
+            .then(data => {
+              console.log(data);
+              if (data.success) {
+                window.location.href = "http://localhost:3000/indice";
+              }
+            })
+            .catch(error => {
+              console.error(error);
+            });
+        
+        
+            
+          } else {
+            // Contraseñas no coinciden, mostrar un mensaje de error
+            errorMessage.innerHTML = 'Las contraseñas no coinciden.';
+          }
+        
+        
+        
+        
+        
+        logIn.addEventListener('click', (e) => {
+          e.preventDefault();
+          e.stopPropagation();
+        
+          const username = document.getElementById('usernameLogIn').value;
+          const password = document.getElementById('password').value;
+        
+          fetch('http://localhost:3000/login', {
+            method: 'POST',
+            credentials: 'include',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              username: username,
+              password: password,
+            })
+          })
+            .then(response => response.json())
+            .then(data => {
+              console.log(data);
+              if (data.success) {
+                  window.location.href = "http://localhost:3000/indice";
+              }
+            })
+            .catch(error => {
+              console.error(error);
+            });
+        });
+});
 });
